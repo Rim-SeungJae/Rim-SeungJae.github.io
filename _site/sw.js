b80da7f262b84e81b95337f17290cce7dec2e0fd
dipreez <calloffate@naver.com>
2024-01-04 15:18:30 +0900

    "24/01/04 fix"

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704346181';
+const cacheName = 'chirpy-1704348977';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
