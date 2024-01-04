0f9596cafd16c87e573c591a4f56616a19a3584b
dipreez <calloffate@naver.com>
2024-01-04 15:16:12 +0900

    "24/01/04 adjustment on categories"

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704263649';
+const cacheName = 'chirpy-1704346181';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
