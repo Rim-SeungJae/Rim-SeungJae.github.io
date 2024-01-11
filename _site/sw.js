2c23da7977e53b79fec636aaf15a648af19de494
dipreez <calloffate@naver.com>
2024-01-11 17:55:26 +0900

    24/01/11 ai project post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704348977';
+const cacheName = 'chirpy-1704963303';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
